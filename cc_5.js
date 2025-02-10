681b215ef9b0c3031471bc0051fd9eb66ed886a8
Jason Velasquez-Lopez <jasonv11@usf.edu>
2025-02-09 20:21:46 -0500

    Task 2- Order Details

--- cc_5.js
+++ cc_5.js
@@ -3,3 +3,10 @@ const customer = { name:"John Pork", age: 42, email: "jpork649@gmail.com"} // De
 console.log(`Customer name: ${customer.name}`); //Logging name
 console.log(`Customer age: ${customer.age}`); //Logging age
 console.log(`Customer email: ${customer.email}`); //Logging email
+
+//Task 2: Object Methods 
+const order = { orderId: 910, totalAmount: 690, status: "In progress",
+    displayOrder: function()
+    {console.log(`Order ID: ${this.orderId}, Total: ${this.totalAmount}, Status: ${this.status}`)}
+ } // Declaring order with properties and adding method 
+order.displayOrder(); // Calling the method 
