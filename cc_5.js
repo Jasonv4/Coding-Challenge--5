9e5561b5074529a62f68c0227738e3c90d053576
Jason Velasquez-Lopez <jasonv11@usf.edu>
2025-02-09 20:00:14 -0500

    Task 1- Customer Profile

--- cc_5.js
+++ cc_5.js
@@ -1,1 +1,5 @@
-
+//Task 1: Object Properties
+const customer = { name:"John Pork", age: 42, email: "jpork649@gmail.com"} // Declaring Customer witn properties
+console.log(`Customer name: ${customer.name}`); //Logging name
+console.log(`Customer age: ${customer.age}`); //Logging age
+console.log(`Customer email: ${customer.email}`); //Logging email
